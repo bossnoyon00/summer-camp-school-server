28e522231e8ce1470e5c2f33e8283d80687181ad
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:36:00 +0600

    add user method

--- index.js
+++ index.js
@@ -61,7 +61,26 @@ async function run() {
 
 
 
-      
+        //jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+
+            res.send({ token })
+        })
+
+
+
+        const instructorVerify = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'instructor') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
 
         const adminVerify = async (req, res, next) => {
             const email = req.decoded.email;
@@ -73,38 +92,7 @@ async function run() {
             next();
         }
 
-        app.get('/users', async (req, res) => {
-            const result = await usersCollection.find().toArray();
-            console.log(result)
-            res.send(result)
-        })
-
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const query = { email: user.email }
-            const existingUser = await usersCollection.findOne(query)
-            if (existingUser) {
-                return res.send({ message: 'user already exist', existingUser })
-            }
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        })
-
-
-        app.patch('/users/admin/:id', async (req, res) => {
-            const id = req.params.id;
-            // console.log(id)
-            const filter = { _id: new ObjectId(id) }
-            const updateDoc = {
-                $set: {
-                    role: 'admin'
-                },
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.send(result)
-
-        })
+     
 
 
         app.patch('/users/instructor/:id', async (req, res) => {
